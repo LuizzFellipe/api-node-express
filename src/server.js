8433bd31b31e60943090238075fac8751b9cc627
LuizzFellipe <luiz.fellipe@aluno.ifsp.edu.br>
2023-08-07 17:54:07 -0300

    Create Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log('Servidor online')
+const express = require('express');
+const api = express();
+const  port = 3000;
+
+api.get('/', (req, res) => {
+    res.json({message: "Hello World"})
+});
+
+api.listen(port, () => {
+    console.log(`Servidor online 127.0.0.1:${port}`);
+});
