87036281c8236d8827a311f7bdd063f2d392df4d
LuizzFellipe <luiz.fellipe@aluno.ifsp.edu.br>
2023-08-28 14:48:00 -0300

    config.js

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@
 import express from 'express';
 import userRouter from './routes/userRouter.js';
 import loginRouter from './routes/loginRouter.js';
+import {PORT} from './config.js';
 
 const api = express();
-const  port = 3000;
 
 api.use('/user', userRouter);
 
 api.use('/auth',loginRouter);
 
-api.listen(port, () => {
-    console.log(`Servidor online 127.0.0.1:${port}`);
+api.listen(PORT, () => {
+    console.log(`Servidor online 127.0.0.1:${PORT}`);
 });
